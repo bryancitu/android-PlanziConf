65cbdb13ce30685b32fd347e6a32a16518ebce93
bryan <bryanjesus.ra@gmail.com>
2020-10-23 05:15:49 -0500

    creando nuestra DB en firebase y subiendo datos a nuestra DB

---
+++ app/src/main/java/com/platzi/platziconf/model/Speaker.kt
@@ -0,0 +1,11 @@
+package com.platzi.platziconf.model
+
+class Speaker {
+    var name = ""
+    var jobtitle = ""
+    var workplace = ""
+    var biography = ""
+    var twitter = ""
+    var image = ""
+    var category = 0
+}
---
+++ app/src/main/java/com/platzi/platziconf/ui/activities/AplashscreenActivity.kt
@@ -0,0 +1,12 @@
+package com.platzi.platziconf.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.platzi.platziconf.R
+
+class AplashscreenActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_aplashscreen)
+    }
+}
