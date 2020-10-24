3b19321f35a54dbf353afc1c6879443f738eeb19
bryan <bryanjesus.ra@gmail.com>
2020-10-24 02:09:54 -0500

    configurando los framentos dialog de speaker y schedule details y registrando nuestro modulo de google maps y agregando en el manifest el metadata para la API de maps

--- app/src/main/java/com/platzi/platziconf/view/ui/fragments/SpeakersDetailDialogFragment.kt
+++ app/src/main/java/com/platzi/platziconf/view/ui/fragments/SpeakersDetailDialogFragment.kt
@@ -1,60 +1,82 @@
 package com.platzi.platziconf.view.ui.fragments
 
+import android.content.Intent
+import android.content.res.ColorStateList
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.DialogFragment
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.platzi.platziconf.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.platzi.platziconf.model.Conference
+import com.platzi.platziconf.model.Speaker
+import kotlinx.android.synthetic.main.fragment_schedule_detail_dialog.*
+import kotlinx.android.synthetic.main.fragment_speakers_detail_dialog.*
+import java.text.SimpleDateFormat
 
 /**
  * A simple [Fragment] subclass.
  * Use the [SpeakersDetailDialogFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class SpeakersDetailDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+class SpeakersDetailDialogFragment : DialogFragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+        setStyle(DialogFragment.STYLE_NORMAL, R.style.FullScreenDialogStyle)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_speakers_detail_dialog, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SpeakersDetailDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SpeakersDetailDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        toolBarSpeaker.navigationIcon = ContextCompat.getDrawable(view.context, R.drawable.ic_close)
+        toolBarSpeaker.setNavigationOnClickListener {
+            dismiss()
+        }
+
+        val  speaker = arguments?.getSerializable("speaker") as Speaker
+        toolBarSpeaker.title = speaker.name
+        toolBarSpeaker.setTitleTextColor(Color.WHITE)
+        tvDetailSpeakerUsername.text = speaker.name
+        tvDetailJobTitle.text = speaker.jobtitle
+        tvDetailTrabajo.text = speaker.workplace
+
+        Glide.with(ivDetailSpeakerImage.context)
+            .load(speaker.image)
+            .apply(RequestOptions.circleCropTransform())
+            .into(ivDetailSpeakerImage)
+
+        ivDetailSpeakerTwitter.setOnClickListener {
+            var intent: Intent
+
+            try {
+                context?.packageManager?.getPackageInfo("com.twitter.android", 0)
+                intent = Intent(Intent.ACTION_VIEW, Uri.parse("twitter://user?screen_name=${speaker.twitter}"))
+                intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+            } catch (e: Exception) {
+                intent = Intent(
+                    Intent.ACTION_VIEW, Uri.parse("https://twitter.com/${speaker.twitter}")
+                )
             }
+            startActivity(intent)
+        }
+
+        tvDetailSpeakerBiography.text = speaker.biography
     }
+
+    override fun onStart() {
+        super.onStart()
+        dialog?.window?.setLayout(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT)
+    }
+
 }
