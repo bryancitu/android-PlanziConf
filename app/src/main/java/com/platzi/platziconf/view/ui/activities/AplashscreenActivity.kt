816851bd01ddfb48a7f5fe37bdd138e79ddbc962
bryan <bryanjesus.ra@gmail.com>
2020-10-24 04:43:51 -0500

    dandole animacion a mi a mi activity splash de inicio

--- app/src/main/java/com/platzi/platziconf/view/ui/activities/AplashscreenActivity.kt
+++ app/src/main/java/com/platzi/platziconf/view/ui/activities/AplashscreenActivity.kt
@@ -1,12 +1,37 @@
 package com.platzi.platziconf.view.ui.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
 import com.platzi.platziconf.R
+import kotlinx.android.synthetic.main.activity_aplashscreen.*
 
 class AplashscreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_aplashscreen)
+
+        val animacion = AnimationUtils.loadAnimation(this,R.anim.animacion)
+        ivPlatziConf.startAnimation(animacion)
+
+        val intent = Intent(this, MainActivity::class.java)
+
+        animacion.setAnimationListener(object: Animation.AnimationListener {
+            override fun onAnimationRepeat(animation: Animation?) {
+
+            }
+
+            override fun onAnimationEnd(animation: Animation?) {
+                startActivity(intent)
+                finish()
+            }
+
+            override fun onAnimationStart(animation: Animation?) {
+
+            }
+
+        })
     }
 }
