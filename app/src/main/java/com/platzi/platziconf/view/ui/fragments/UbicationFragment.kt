d511b0e22990d844c74f7fc3550efc75aa5fee92
bryan <bryanjesus.ra@gmail.com>
2020-10-24 02:40:14 -0500

    personalizando el punto de inicio y agregandole un icono en el point de googlemaps

--- app/src/main/java/com/platzi/platziconf/view/ui/fragments/UbicationFragment.kt
+++ app/src/main/java/com/platzi/platziconf/view/ui/fragments/UbicationFragment.kt
@@ -1,14 +1,19 @@
 package com.platzi.platziconf.view.ui.fragments
 
+import android.graphics.Bitmap
+import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
+import androidx.core.content.ContextCompat
+import com.google.android.gms.maps.*
+import com.google.android.gms.maps.model.BitmapDescriptorFactory
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.platzi.platziconf.R
+import com.platzi.platziconf.model.Ubication
 
 /**
  * A simple [Fragment] subclass.
@@ -28,7 +33,24 @@ class UbicationFragment : Fragment(), OnMapReadyCallback {
         mapFragment.getMapAsync(this)
     }
 
-    override fun onMapReady(p0: GoogleMap?) {
+    override fun onMapReady(googleMap: GoogleMap) {
+        val ubication = Ubication()
 
+        val zoom = 16f
+        val centerMap = LatLng(ubication.latitud, ubication.longitud)
+
+        googleMap?.animateCamera(CameraUpdateFactory.newLatLngZoom(centerMap, zoom))
+
+        val centerMark = LatLng(ubication.latitud,ubication.longitud)
+        val marketOptions = MarkerOptions()
+        marketOptions.position(centerMark)
+        marketOptions.title("Platzi Conf 2019")
+
+        val bitmapDraw = context?.applicationContext?.let { ContextCompat.getDrawable(it, R.drawable.logo_platzi) } as BitmapDrawable
+        val smallMaker = Bitmap.createScaledBitmap(bitmapDraw.bitmap, 150,150, false)
+
+        marketOptions.icon(BitmapDescriptorFactory.fromBitmap(smallMaker))
+
+        googleMap.addMarker(marketOptions)
     }
 }
